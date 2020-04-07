2edcaacece729ed73a20e945aecdac3cc016ac6f
miguel melgar <46229988+melgar1992@users.noreply.github.com>
2020-04-06 22:57:40 -0400

    Facturacion prueba
    Se agrego una condicion cuando no se encuentran los detalles de la empresa de la base de datos

--- application/controllers/Movimientos/Facturacion.php
+++ application/controllers/Movimientos/Facturacion.php
@@ -25,18 +25,24 @@ class Facturacion extends BaseController
         $monto = $this->input->post('monto_total');
         $llave_dosificada = $this->input->post('llave_dosificacion');
         $datosFacturaEmpresa = $this->Empresa_model->getDatosFactura();
-        $fecha = str_replace('-', '', $fecha);
-        $controlCode = new ControlCode();
-        $codigoControl = $controlCode->generate($num_autorizacion, $num_factura, $nitci, $fecha, $monto, $llave_dosificada);
-        $qr = new QR_BarCode();
-        $fechaQr = date("d/m/Y", strtotime($fecha));
-        $qr->text($datosFacturaEmpresa->nit_ci . '|' . $num_factura . '|' . $num_autorizacion . '|' . $fecha . '|' . $fechaQr . '|' . $monto . '|' . $monto . '|' . $codigoControl . '|' . $nitci . '|0|0|0|0');
-        $qr->qrCode(200, 'assets/img/Codigos_Qr/' . $num_factura . '.png');
+        if (isset($datosFacturaEmpresa)) {
+            $fecha = str_replace('-', '', $fecha);
+            $controlCode = new ControlCode();
+            $codigoControl = $controlCode->generate($num_autorizacion, $num_factura, $nitci, $fecha, $monto, $llave_dosificada);
+            $qr = new QR_BarCode();
+            $fechaQr = date("d/m/Y", strtotime($fecha));
+            $qr->text($datosFacturaEmpresa->nit_ci . '|' . $num_factura . '|' . $num_autorizacion . '|' . $fecha . '|' . $fechaQr . '|' . $monto . '|' . $monto . '|' . $codigoControl . '|' . $nitci . '|0|0|0|0');
+            $qr->qrCode(200, 'assets/img/Codigos_Qr/' . $num_factura . '.png');
 
-        $data = array(
-            'codigoControl' => $codigoControl,
-            'codigoQrUrl' => 'assets/img/Codigos_Qr/' . $num_factura . '.png',
-        );
-        $this->loadView('Ventas', '/form/admin/ventas/facturacion_prueba', $data);
+            $data = array(
+                'codigoControl' => $codigoControl,
+                'codigoQrUrl' => 'assets/img/Codigos_Qr/' . $num_factura . '.png',
+            );
+            $this->loadView('Ventas', '/form/admin/ventas/facturacion_prueba', $data);
+        } else {
+            $this->session->set_flashdata("error", "No se puede generar la factura, por no se a detallado la informacion de la empresa");
+            $data = array();
+            $this->loadView('Ventas', '/form/admin/ventas/facturacion_prueba', $data);
+        }
     }
 }
